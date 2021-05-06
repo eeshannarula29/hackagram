f958a279cc99da37e967ab66855e90f07f7a7bfa
eeshannarula29 <eeshannarula29@gmail.com>
2021-05-06 17:35:44 +0530

    added refresh token

--- view_controller.py
+++ view_controller.py
@@ -1,14 +1,26 @@
 """File controls the application while it is running"""
+
+import time
+
 from components.loding_screen import *
 from firebase_manager.handler import Handler
 from keys import keys
 
 
 def run() -> None:
+    start_time = time.time()
+    threshold_time = 15 * 60  # 15 minutes in secs
     handler = Handler(keys.keys_auth, keys.keys_database)
     curr_view = LoadingView(handler=handler)
 
     while curr_view:
+        current_time = time.time()
+        time_diff = current_time - start_time
+
+        if threshold_time < time_diff:
+            handler.refresh()
+            start_time = time.time()
+
         curr_view = curr_view.present()
 
 
